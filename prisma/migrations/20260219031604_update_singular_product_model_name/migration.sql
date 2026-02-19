87f695f87596348e79042f8b6afedf084cb7f696
Tux G. <edgoca2498@gmail.com>
2026-02-18 21:28:06 -0600

    chore: Rename singular product model name

---
+++ prisma/migrations/20260219031604_update_singular_product_model_name/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Products` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Products";
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "slug" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "images" TEXT[],
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
--- prisma/seed/seed-database.ts
+++ prisma/seed/seed-database.ts
@@ -1,17 +1,17 @@
 
 import getPrismaClient from '../../server/utils/prisma.js';
-import { products } from "./product.seed.js";
+import { products } from "./products.seed.js";
 import { siteReviewData } from "./siteReviews.seed.js";
 
 
 const prisma = getPrismaClient();
 
 (async () => {
-  await prisma.products.deleteMany();
+  await prisma.product.deleteMany();
   await prisma.siteReview.deleteMany();
   
   for (const product of products) {
-    await prisma.products.create({ data: product });
+    await prisma.product.create({ data: product });
   }
 
   for (const review of siteReviewData) {
